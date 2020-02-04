b98a333e8cb4b803fa24eb19f52ed6700b367120
kokorii <kokori91@gmail.com>
2020-02-04 21:48:08 +0900

    인라인 키보드 메뉴 입력/콜백 처리

---
+++ index.js
@@ -0,0 +1,51 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Listener (handler) for telegram's /label event
+bot.onText(/\/label/, function(msg, match){
+  const chatId = msg.chat.id;
+  const url = match.input.split(' ')[1];
+
+  if (url === undefined) {
+      bot.sendMessage(
+          chatId,
+          'Please provide URL of article!',
+      );
+    return;
+  }
+
+  bot.sendMessage(
+    chatId,
+    'this is callback data',
+    {
+        reply_markup: {
+            inline_keyboard: [[
+                {
+                    text: 'Opt1',
+                    callback_data: 'Opt1'
+                },
+                {
+                  text: 'Opt2',
+                  callback_data: 'Opt2'
+                },
+                {
+                    text: 'Other',
+                    callback_data: 'other'
+                }
+            ]]
+        }
+    });
+});
+
+// Listener (handler) for callback data from /label command
+bot.on('callback_query', function(query) {
+  const message = query.message;
+  const category = query.data;
+
+  bot.sendMessage(message.chat.id,`clicked ${category}`);
+});
