cf38e8301b89eb1634c64d28ca156e8fdb78aef6
kokorii <kokori91@gmail.com>
2020-02-05 22:18:32 +0900

    parmeter msg 추가

--- bot.js
+++ bot.js
@@ -41,7 +41,7 @@ bot.onText(/\/new/, function(msg, match){
 });
  
 console.log('schedule');
-var j = schedule.scheduleJob('*/1 * * * *', function(){
+var j = schedule.scheduleJob('*/1 * * * *', function(msg){
     
     bot.sendMessage(msg.chat.id, "그 순간 소중한 것이 영원히 소중한 것은 아니다. ");
     console.log("success");
