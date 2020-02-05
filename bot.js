cbe299acd4bfcdd09f11aeedfe2d4fa0944ecfda
kokorii <kokori91@gmail.com>
2020-02-05 17:55:55 +0900

    /start handler 추가

--- bot.js
+++ bot.js
@@ -12,12 +12,13 @@ const getToken = (function(){
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(getToken(), {polling: true});
 
-// Listener (handler) for telegram's any kind of message.
-bot.on('message', (msg) => {
+// Listener (handler) for /start
+bot.onText('/\start/', (msg) => {
     const chatId = msg.chat.id;
-  
+    const user_name = msg.from.first_name;
+
     // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, '안녕하세요, \"\/new 오늘의 한마디\"로 새 글귀를 저장해보세요.');
+    bot.sendMessage(chatId, `안녕하세요 ${user_name}님,<br> \"\/new 오늘의 한마디\"로 새 글귀를 저장해보세요.`);
   });
   
 bot.onText(/\/new/, function(msg, match){
@@ -33,7 +34,7 @@ bot.onText(/\/new/, function(msg, match){
     }
 
     // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId,`\"${user_msg}\"가 저장되었습니다.`);
+    bot.sendMessage(chatId,`\"${user_msg}\"가 저장되었습니다. <br> 하루 한 번 저장된 메세지중 하나를 보내드릴게요.`);
     
 });
 
