bbd43ae7d000b1fab133ce57476571e5d99f8987
kokorii <kokori91@gmail.com>
2020-02-05 22:32:20 +0900

    메세지만보내기

--- bot.js
+++ bot.js
@@ -42,10 +42,14 @@ bot.onText(/\/new/, function(msg, match){
  
 console.log('schedule');
 var j = schedule.scheduleJob('*/1 * * * *', function(msg){
+    bot.on((msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, "그 순간 소중한 것이 영원히 소중한 것은 아니다. ");
-    console.log("success");
+    bot.sendMessage(msg.chat.id, "그 순간 소중한 것이 영원히 소중한 것은 아니다. ");
+
 });
+
+});
+
 
 // Listener (handler) for telegram's /test event
 bot.onText(/\/test/, function(msg, match){
